ed34a082e58511d6c9eb62ed3ad96f488dac29b9
Tim Strazzere <tim@strazzere.com>
2022-12-07 16:46:10 -0800

    Lint day 6

--- src/2022/6/driver.ts
+++ src/2022/6/driver.ts
@@ -1,45 +1,45 @@
 export function work(input: string): number {
-  let splitInput = input.split('')
-  let start = -1
-  let windowSize = 4
+  const splitInput = input.split('');
+  let start = -1;
+  const windowSize = 4;
 
   splitInput.some((_value, index) => {
     if (index > input.length - windowSize) {
-      return false
+      return false;
     }
     for (let i = 1; i < windowSize; i++) {
-      let window = splitInput.slice(index + i, index + windowSize)
+      const window = splitInput.slice(index + i, index + windowSize);
       if (window.includes(splitInput[index + i - 1])) {
-        return false
+        return false;
       }
     }
 
-    start = index + windowSize
-    return true
-  })
+    start = index + windowSize;
+    return true;
+  });
 
-  return start
+  return start;
 }
 
 export function work2(input: string): number {
-  let splitInput = input.split('')
-  let start = -1
-  let windowSize = 14
+  const splitInput = input.split('');
+  let start = -1;
+  const windowSize = 14;
 
   splitInput.some((_value, index) => {
     if (index > input.length - windowSize) {
-      return false
+      return false;
     }
     for (let i = 1; i < windowSize; i++) {
-      let window = splitInput.slice(index + i, index + windowSize)
+      const window = splitInput.slice(index + i, index + windowSize);
       if (window.includes(splitInput[index + i - 1])) {
-        return false
+        return false;
       }
     }
 
-    start = index + windowSize
-    return true
-  })
+    start = index + windowSize;
+    return true;
+  });
 
-  return start
+  return start;
 }
