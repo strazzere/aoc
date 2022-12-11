c35cbfae131e31ea78411d154d48083ddf39abcc
Tim Strazzere <tim@strazzere.com>
2022-12-11 15:45:56 -0800

    day 11 part 2

--- src/2022/11/driver.spec.ts
+++ src/2022/11/driver.spec.ts
@@ -20,43 +20,19 @@ describe('day 11', () => {
     });
   });
 
-  //   describe('part 2', () => {
-  //     it('can parse test inputs correctly', () => {
-  //       const input = fs.readFileSync('./src/2022/11/test.txt', 'utf8');
-  //       const underTest = work2(input);
-  //       const expected = `##..##..##..##..##..##..##..##..##..##..
-  // ###...###...###...###...###...###...###.
-  // ####....####....####....####....####....
-  // #####.....#####.....#####.....#####.....
-  // ######......######......######......###.
-  // #######.......#######.......#######.....
-  // `;
-  //       // It actually should be the following, we are off by one, but it still is readable.
-  //       // It's painful to know a bug is still there but to be honest, it works "enough", alas
-  //       // just like a real solution in the wild, hah
-  //       // ##..##..##..##..##..##..##..##..##..##..
-  //       // ###...###...###...###...###...###...###.
-  //       // ####....####....####....####....####....
-  //       // #####.....#####.....#####.....#####.....
-  //       // ######......######......######......####
-  //       // #######.......#######.......#######.....
+  describe('part 2', () => {
+    it('can parse test inputs correctly', () => {
+      const input = fs.readFileSync('./src/2022/11/test.txt', 'utf8');
+      const underTest = work2(input);
+      const expected = 2713310158;
+      expect(underTest, 'should be identical').to.deep.equal(expected);
+    });
 
-  //       expect(underTest, 'should be identical').to.deep.equal(expected);
-  //     });
-  //     it('can parse input2 correctly', () => {
-  //       const input = fs.readFileSync('./src/2022/11/input2.txt', 'utf8');
-  //       const underTest = work2(input);
-  //       const expected = `###..###..###...##..###...##...##..#####
-  // #..#.#..#.#..#.#..#.#..#.#..#.#..#.#...#
-  // #..#.###..#..#.#..#.#..#.#..#.#....###.#
-  // ###..#..#.###..####.###..####.#.##.#....
-  // #.#..#..#.#....#..#.#.#..#..#.#..#.#...#
-  // #..#.###..#....#..#.#..#.#..#..###.#....
-  // `;
-  //       // console.log(underTest) // RBPARAGF
-  //       // Like mentioned about, there is a bug in the last row, but it is still readable
-  //       // so I'll come back to it some day, maybe
-  //       expect(underTest, 'should be identical').to.deep.equal(expected);
-  //     });
-  //   });
+    it('can parse input2 correctly', () => {
+      const input = fs.readFileSync('./src/2022/11/input2.txt', 'utf8');
+      const underTest = work2(input);
+      const expected = 14952185856;
+      expect(underTest, 'should be identical').to.deep.equal(expected);
+    });
+  });
 });
--- src/2022/11/driver.ts
+++ src/2022/11/driver.ts
@@ -74,7 +74,7 @@ export function work(input: string): number {
         monkey.inspected++;
         switch (monkey.operation) {
           case Operation.ADDITION:
-            item += monkey.operationNum;
+            item = item + monkey.operationNum;
             break;
           case Operation.SUBTRACTION:
             item -= monkey.operationNum;
@@ -86,7 +86,7 @@ export function work(input: string): number {
             item *= monkey.operationNum;
             break;
           case Operation.POWER_TWO:
-            item = Math.pow(item, monkey.operationNum);
+            item *= item;
             break;
           default:
             throw new Error('Unknown Operation');
@@ -104,15 +104,121 @@ export function work(input: string): number {
     });
   }
 
-  console.log(monkeys);
-
   const topMonkeys = monkeys.sort((x, y) => y.inspected - x.inspected).slice(0, 2);
 
   return topMonkeys[0].inspected * topMonkeys[1].inspected;
 }
 
+const gcdOverTwo = (x = 0, y = 0): number => {
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while (y) {
+    const t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+};
+
+const gcd = (x?: number | number[], y?: number): number =>
+  Array.isArray(x) ? x.reduce((a, n) => gcdOverTwo(a, n), 1) : gcdOverTwo(x, y);
+
+const lcmOverTwo = (x?: number, y?: number): number =>
+  !x || !y ? 0 : Math.abs((x * y) / gcd(x, y));
+
+const lcm = (x?: number | number[], y?: number): number =>
+  Array.isArray(x) ? x.reduce((a, n) => lcmOverTwo(a, n), 1) : lcmOverTwo(x, y);
+
 export function work2(input: string): number {
   const instructions = input.split('\n');
 
-  return -1;
+  const monkeys: Monkey[] = [];
+
+  let monkey: Monkey = undefined;
+  instructions.map((instruction) => {
+    if (instruction.includes('Monkey')) {
+      monkey = new Monkey();
+    } else if (instruction.includes('Starting items')) {
+      monkey.items = instruction
+        .slice(18)
+        .split(', ')
+        .map((val) => Number(val));
+    } else if (instruction.includes('Operation')) {
+      const tmp = instruction.slice(23).split(' ');
+      if (tmp[0] === Operation.MULTI && tmp[1] === 'old') {
+        monkey.operation = Operation.POWER_TWO;
+        monkey.operationNum = 2;
+      } else {
+        switch (tmp[0]) {
+          case Operation.MULTI:
+            monkey.operation = Operation.MULTI;
+            break;
+          case Operation.DIV:
+            monkey.operation = Operation.DIV;
+            break;
+          case Operation.ADDITION:
+            monkey.operation = Operation.ADDITION;
+            break;
+          case Operation.SUBTRACTION:
+            monkey.operation = Operation.SUBTRACTION;
+            break;
+          default:
+            throw new Error('Unknown operation');
+        }
+        monkey.operationNum = Number(tmp[1]);
+      }
+    } else if (instruction.includes('Test:')) {
+      monkey.divisibleTest = Number(instruction.slice(21));
+    } else if (instruction.includes('If true:')) {
+      monkey.toTrue = Number(instruction.split(' ').at(-1));
+    } else if (instruction.includes('If false:')) {
+      monkey.toFalse = Number(instruction.split(' ').at(-1));
+    } else {
+      monkey.inspected = 0;
+      monkeys.push(monkey);
+    }
+  });
+
+  const lowestCommon: number = lcm(monkeys.map((monkey) => monkey.divisibleTest));
+
+  const rounds = 10000;
+  for (let i = 0; i < rounds; i++) {
+    monkeys.map((monkey) => {
+      monkey.items.map((item) => {
+        monkey.inspected++;
+        switch (monkey.operation) {
+          case Operation.ADDITION:
+            item += monkey.operationNum;
+            break;
+          case Operation.SUBTRACTION:
+            item -= monkey.operationNum;
+            break;
+          case Operation.DIV:
+            item /= monkey.operationNum;
+            break;
+          case Operation.MULTI:
+            item *= monkey.operationNum;
+            break;
+          case Operation.POWER_TWO:
+            item = Math.pow(item, 2);
+            break;
+          default:
+            throw new Error('Unknown Operation');
+        }
+
+        let to = monkey.toFalse;
+        if (item % monkey.divisibleTest === 0) {
+          to = monkey.toTrue;
+        }
+
+        monkeys[to].items.push(item % lowestCommon);
+      });
+      monkey.items = [];
+    });
+  }
+
+  const topMonkeys = monkeys.sort((x, y) => y.inspected - x.inspected).slice(0, 2);
+
+  console.log(topMonkeys);
+  return topMonkeys[0].inspected * topMonkeys[1].inspected;
 }
